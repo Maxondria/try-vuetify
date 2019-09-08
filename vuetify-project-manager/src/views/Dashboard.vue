b05fbf363e8be18d293c046ab920f25c2136b0cc
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-09 01:01:43 +0300

    [task] Added margins on main content, and specific component margin

---
+++ vuetify-project-manager/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1 class="subheading grey--text">Dashboard</h1>
+
+    <v-container class="my-5">
+      content
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
