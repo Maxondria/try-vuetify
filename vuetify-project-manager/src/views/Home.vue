87e4010864aa3693efbabe37939b3ee5f6518e63
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-09-08 17:38:17 +0300

    [task] created a default navbar-toolbar-component and removed boilerplate code

--- vuetify-project-manager/src/views/Home.vue
+++ vuetify-project-manager/src/views/Home.vue
@@ -1,13 +1,10 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Home Page</h1>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld';
-
-export default {
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
